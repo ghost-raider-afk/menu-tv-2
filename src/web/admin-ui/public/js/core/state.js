10c0bebfda09a3b4bcf5ccd0af65df626169058b
ghost-raider-afk <michaelfedoseev2978@gmail.com>
2026-08-15 16:57:07 +1000

    Add frontend application state

---
+++ src/web/admin-ui/public/js/core/state.js
@@ -0,0 +1,16 @@
+export const state = {
+  session: null,
+  user: null,
+  site: null,
+  notificationTimer: null,
+  locations: [],
+  screens: [],
+  templates: [],
+  products: [],
+  packaging: [],
+  sftpDirectories: [],
+  editingLocationId: null,
+  editingTemplateId: null,
+  editingProductId: null,
+  editingPackagingId: null
+};
