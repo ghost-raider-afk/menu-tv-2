d03d39b1204efd62d62204c04a0802fe586d2767
ghost-raider-afk <michaelfedoseev2978@gmail.com>
2026-08-20 11:15:59 +1000

    Apply login presentation atomically after public config loads

--- src/web/admin-ui/public/js/pages/signin.js
+++ src/web/admin-ui/public/js/pages/signin.js
@@ -3,10 +3,19 @@ import { api } from '../core/api.js';
 import { element, setMessage, setPending } from '../core/dom.js';
 import { applyPresentation } from '../core/presentation.js';
 
+function revealPresentation() {
+  document.documentElement.dataset.signinPresentation = 'ready';
+}
+
 export function initialiseSignIn() {
   const form = element('signin-form');
   if (!(form instanceof HTMLFormElement)) return;
-  void api.get(API.publicConfig).then(applyPresentation).catch(() => undefined);
+
+  void api.get(API.publicConfig)
+    .then(applyPresentation)
+    .catch(() => undefined)
+    .finally(revealPresentation);
+
   void api.get(API.session).then(() => window.location.replace('/')).catch(() => undefined);
   form.addEventListener('submit', async (event) => {
     event.preventDefault();
